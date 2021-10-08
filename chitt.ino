2a84ca757751dafbac4cad39480fd12d397c932d
Bindu Sri Amballa <bindusri.amballa2000@gmail.com>
2021-10-08 16:25:30 +0530

    final code

---
+++ chitt.ino
@@ -0,0 +1,68 @@
+#include <Servo.h>     //declare ServoTimer2 library to run servo with TMRpcm
+#include"SD.h"                //declare SD library for SD cards
+#define SD_ChipSelectPin 10   //select sd chip select pin 10
+#include"TMRpcm.h"            //library to play audio from sd card by arduino
+#include"SPI.h"              //create Serial peripheral Interface Communication
+TMRpcm tmrpcm;              //name tmrpcm
+Servo head;           //declare Head servo followed by left right hand servos
+Servo lhand;
+Servo rhand;
+
+int in1= 6;               //declare Infrared sensor pins
+int in2= 7;
+int in3= 8;
+
+void setup() 
+{
+ head.attach(2);        //attach servos to pins
+ lhand.attach(3);
+ rhand.attach(4);
+ tmrpcm.speakerPin=9;   //Speaker pin 9,audio out pin is 9
+ 
+ Serial.begin(9600);    //initialize Serial communication
+ 
+ if(!SD.begin(SD_ChipSelectPin))   //this runs until SD is connected
+ {
+  Serial.println("SD FAIL");
+  return; 
+ }
+
+}
+
+void loop() 
+{
+  if(!(digitalRead(in3)))          //Chest sensor is low- Say "Hi there.."
+  {
+   tmrpcm.play("hello chitti.wav");     //Playing audio saved as 'welcome.wav'
+    lhand.write(2500);            //hand up- saying HI
+    delay(2000);
+    lhand.write(1200);            //hand comes to handshake position
+    
+    delay(9000);
+    
+  }
+ 
+  if(!(digitalRead(in1)))       //Sensor on eye gets low play audio -"dont touch my eye"
+  {
+    tmrpcm.play("dont.wav");     //play file saved as "dont.wav" - dont touch my eye 
+    rhand.write(300);            //hand up
+    head.write(2000);
+    delay(3000);
+  }
+ 
+  if(!(digitalRead(in2)))     //Sensor on eye gets low play audio -"dont touch my eye"
+  {
+    tmrpcm.play("dont.wav");
+    lhand.write(2000);
+    head.write(1000);
+    delay(3000);
+  }
+ 
+ else             //No input detected - keep the robot steady.
+ {
+  
+  lhand;
+  rhand;
+  head;
+ }
+}
